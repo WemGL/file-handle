fe917fe318e79eddf7f646d5c6d49d23ce135be7
Wembley Leach <wembley.gl@gmail.com>
2016-05-04 22:03:02 -0400

    Add about scene and fxml layout. Also, fix installer so it shows the file handle logo.

---
+++ src/main/java/com/wembleyleach/filehandle/AboutController.java
@@ -0,0 +1,18 @@
+package com.wembleyleach.filehandle;
+
+import javafx.fxml.*;
+import javafx.scene.image.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AboutController implements Initializable {
+
+    @FXML
+    private ImageView logo;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        logo.setImage(new Image("/thirds-logo.png"));
+    }
+}
